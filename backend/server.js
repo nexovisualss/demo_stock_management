a656ff8c95dc6e5193b269cd84814486754ac0db
nexovisualss <nexovisualss@gmail.com>
2026-02-26 09:49:30 +0530

    fix backend package.json + render deploy fix

--- backend/server.js
+++ backend/server.js
@@ -27,5 +27,5 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 // server start
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
